d257d1746ccc3f831f0c0407a8096bccb94e199b
Mark Wilde <markwilde29@gmail.com>
2020-04-15 14:45:02 +0100

    Set some variables.

--- app.js
+++ app.js
@@ -9,3 +9,16 @@ next player's turn
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+// set variables
+var scores, roundScore, activePlayer, dice;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+dice = Math.floor(Math.random() * 6) + 1;
+console.log(dice);
+
+// get the element of the selected class
+document.querySelector('#current-' + activePlayer).textContent = dice;
